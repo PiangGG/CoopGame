76b7adcef8cf54d988ae8287137a64a08ede3d73
piang <974805475@qq.com>
2020-09-25 22:24:59 +0800

    Hitpoints and Death

--- GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
+++ GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
@@ -11,18 +11,22 @@
 #endif
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
+	GOOPGAME_API UFunction* Z_Construct_UDelegateFunction_GoopGame_OnHealthChanged__DelegateSignature();
 	UPackage* Z_Construct_UPackage__Script_GoopGame()
 	{
 		static UPackage* ReturnPackage = nullptr;
 		if (!ReturnPackage)
 		{
+			static UObject* (*const SingletonFuncArray[])() = {
+				(UObject* (*)())Z_Construct_UDelegateFunction_GoopGame_OnHealthChanged__DelegateSignature,
+			};
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/GoopGame",
-				nullptr,
-				0,
+				SingletonFuncArray,
+				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x83092D45,
-				0xDF2717FC,
+				0x3079D5E5,
+				0x50427BB9,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
