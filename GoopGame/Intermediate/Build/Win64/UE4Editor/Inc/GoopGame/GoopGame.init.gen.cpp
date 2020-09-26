8eccd71687bbcf7793060c7d7930e461a1da3d76
piang <974805475@qq.com>
2020-09-27 00:07:45 +0800

    Game Networking

--- GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
+++ GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
@@ -25,8 +25,8 @@ void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x3079D5E5,
-				0x50427BB9,
+				0x0386040E,
+				0x6DB26166,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
