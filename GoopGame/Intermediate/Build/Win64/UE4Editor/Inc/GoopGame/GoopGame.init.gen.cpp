10bab6b4a6c74a0d1f32788869f1f86d9b7a5d22
piang <974805475@qq.com>
2020-09-30 13:11:18 +0800

    GameMode Setup (2)

--- GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
+++ GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
@@ -25,8 +25,8 @@ void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x33534656,
-				0xECDF530F,
+				0x3F7260E5,
+				0xA5AC1DE5,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
