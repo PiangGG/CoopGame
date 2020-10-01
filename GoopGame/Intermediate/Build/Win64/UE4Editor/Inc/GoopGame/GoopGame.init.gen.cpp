c583ea614f4f3c8fd00901ba1ee59c98b1428621
piang <974805475@qq.com>
2020-10-01 13:42:17 +0800

    Game Mode (完结)

--- GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
+++ GoopGame/Intermediate/Build/Win64/UE4Editor/Inc/GoopGame/GoopGame.init.gen.cpp
@@ -11,6 +11,7 @@
 #endif
 PRAGMA_DISABLE_DEPRECATION_WARNINGS
 void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
+	GOOPGAME_API UFunction* Z_Construct_UDelegateFunction_GoopGame_OnActorKilled__DelegateSignature();
 	GOOPGAME_API UFunction* Z_Construct_UDelegateFunction_GoopGame_OnHealthChanged__DelegateSignature();
 	UPackage* Z_Construct_UPackage__Script_GoopGame()
 	{
@@ -18,6 +19,7 @@ void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
 		if (!ReturnPackage)
 		{
 			static UObject* (*const SingletonFuncArray[])() = {
+				(UObject* (*)())Z_Construct_UDelegateFunction_GoopGame_OnActorKilled__DelegateSignature,
 				(UObject* (*)())Z_Construct_UDelegateFunction_GoopGame_OnHealthChanged__DelegateSignature,
 			};
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
@@ -25,8 +27,8 @@ void EmptyLinkFunctionForGeneratedCodeGoopGame_init() {}
 				SingletonFuncArray,
 				UE_ARRAY_COUNT(SingletonFuncArray),
 				PKG_CompiledIn | 0x00000000,
-				0x3F7260E5,
-				0xA5AC1DE5,
+				0xA41CC89C,
+				0x46E78D28,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
